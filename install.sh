3c8a44845090e0dc6bbe7429ac2a9ad3fb357fee
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2018-08-24 15:06:31 +0900

    Add tmux setting.

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ ln -s $(realpath ./vim/vimrc) ~/.config/nvim/init.vim
 mkdir -p ~/.pip
 ln -s $(realpath ./pip/pip.conf) ~/.pip/
 
+ln -s $(realpath ./tmux/tmux.conf) ~/.tmux.conf
+
 cat ./bash/bashrc >> ~/.bashrc
