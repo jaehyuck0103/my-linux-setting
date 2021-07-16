507a95fed7229cacb3d7a28a761556580e7a5452
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2021-07-16 17:09:44 +0900

    pylintrc와 clang-format을 ~/Projects/에도 배치

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ ln -s $(realpath ./tmux/tmux.conf) ~/.tmux.conf
 ln -s $(realpath ./libinput-gestures/libinput-gestures.conf) ~/.config/
 
 ln -s $(realpath ./pylint/pylintrc) ~/.pylintrc
-
+ln -s $(realpath ./pylint/pylintrc) ~/Projects/.pylintrc
 ln -s $(realpath ./clang-format/clang-format) ~/.clang-format
+ln -s $(realpath ./clang-format/clang-format) ~/Projects/.clang-format
 
 cat ./bash/bashrc >> ~/.bashrc
