b1ebc428f5ed23a64f881456af148d0b97483cf1
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2021-07-16 14:19:30 +0900

    Remove flake8, vnc.

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ ln -s $(realpath ./pip/pip.conf) ~/.pip/
 
 ln -s $(realpath ./tmux/tmux.conf) ~/.tmux.conf
 
-ln -s $(realpath ./flake8/flake8) ~/.config/flake8
-
 ln -s $(realpath ./libinput-gestures/libinput-gestures.conf) ~/.config/
 
 ln -s $(realpath ./pylint/pylintrc) ~/.pylintrc
