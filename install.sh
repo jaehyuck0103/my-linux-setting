f8af90c9f87c1fdcc1ba0e0a49b30436d40e71ab
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2021-07-27 17:50:23 +0900

    tiny update

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 mkdir -p ~/.config/nvim
-ln -s $(realpath ./vim/vimrc) ~/.config/nvim/init.vim
-ln -s $(realpath ./vim/coc-settings.json) ~/.config/nvim/
+ln -sf $(realpath ./vim/vimrc) ~/.config/nvim/init.vim
+ln -sf $(realpath ./vim/coc-settings.json) ~/.config/nvim/
 
 # mkdir -p ~/.pip
-# ln -s $(realpath ./pip/pip.conf) ~/.pip/
+# ln -sf $(realpath ./pip/pip.conf) ~/.pip/
 
-ln -s $(realpath ./tmux/tmux.conf) ~/.tmux.conf
+ln -sf $(realpath ./tmux/tmux.conf) ~/.tmux.conf
 
-ln -s $(realpath ./libinput-gestures/libinput-gestures.conf) ~/.config/
+ln -sf $(realpath ./libinput-gestures/libinput-gestures.conf) ~/.config/
 
-ln -s $(realpath ./pylint/pylintrc) ~/.pylintrc
-ln -s $(realpath ./pylint/pylintrc) ~/Projects/.pylintrc
-ln -s $(realpath ./clang-format/clang-format) ~/.clang-format
-ln -s $(realpath ./clang-format/clang-format) ~/Projects/.clang-format
+ln -sf $(realpath ./pylint/pylintrc) ~/.pylintrc
+ln -sf $(realpath ./pylint/pylintrc) ~/Projects/.pylintrc
+ln -sf $(realpath ./clang-format/clang-format) ~/.clang-format
+ln -sf $(realpath ./clang-format/clang-format) ~/Projects/.clang-format
 
 cat ./bash/bashrc >> ~/.bashrc
