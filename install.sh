e3a4a28952d9bad40e035b833096f85d418c0f70
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2022-04-11 04:01:23 +0900

    Auto pull my-linux-setting. Source bashrc instead appeding.

--- install.sh
+++ install.sh
@@ -12,4 +12,14 @@ ln -sf $(realpath ./pylint/pylintrc) ~/Projects/.pylintrc
 ln -sf $(realpath ./clang-format/clang-format) ~/.clang-format
 ln -sf $(realpath ./clang-format/clang-format) ~/Projects/.clang-format
 
-cat ./bash/bashrc >> ~/.bashrc
+
+if grep -q "# source my-linux-setting" ~/.bashrc; then
+    echo "The bashrc already contains my-linux-setting."
+else
+    echo "Add my-linux-setting to bashrc."
+
+    echo "" >> ~/.bashrc
+    echo "" >> ~/.bashrc
+    echo "# source my-linux-setting" >> ~/.bashrc
+    echo "source ~/Utils/my-linux-setting/bash/bashrc" >> ~/.bashrc
+fi
