12e510f78df9816253e7b59dee179cd1b6e472e7
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2023-03-08 14:55:18 +0900

    Update kanagawa setting.

--- nvim/lua-config/kanagawa.lua
+++ nvim/lua-config/kanagawa.lua
@@ -3,10 +3,11 @@
 -- this will affect all the hl-groups where the redefined colors are used
 local my_colors = {
 	-- use the palette color name...
-	sumiInk1 = "#1C1C1C",
+	sumiInk3 = "#1C1C1C",
+	sumiInk4 = "#1C1C1C",
 	fujiWhite = "#D0D0D0",
 	carpYellow = "#D0D0D0",
 }
 
-require("kanagawa").setup({ colors = my_colors })
+require("kanagawa").setup({ colors = { palette = my_colors }})
 vim.cmd("colorscheme kanagawa")
