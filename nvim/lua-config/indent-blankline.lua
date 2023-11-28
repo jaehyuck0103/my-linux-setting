4681e7bf1e487b65ad0961b1bbe7a336ec8fc37e
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2023-11-28 21:38:32 +0900

    nvim indent-blankline v3용 config

--- nvim/lua-config/indent-blankline.lua
+++ nvim/lua-config/indent-blankline.lua
@@ -1,9 +1,17 @@
 -- color를 Yggdroot/indentLine default랑 똑같이 맞추었다.
--- kanagawa plugin에서 overwrite하므로, kanagawa 뒤에서 실행할 것.
-vim.cmd [[highlight IndentBlanklineChar guifg=grey30 gui=nocombine]]
+-- kanagawa plugin에서 overwrite하므로, kanagawa 뒤에서 실행할 것. (v2일때 얘기.. IndentBlanklineChar 사용했었음)
+-- v3에서는 IblIndent 사용
+vim.cmd [[highlight IblIndent guifg=grey30 gui=nocombine]]
 
-require("indent_blankline").setup {
-    char = '¦',
+require("ibl").setup {
+    indent = {char = '¦'},
     -- char_blankline = ' ',
-    show_first_indent_level = false,
+    -- show_first_indent_level = false,
+    scope = {enabled = false},
 }
+
+local hooks = require "ibl.hooks"
+hooks.register(
+    hooks.type.WHITESPACE,
+    hooks.builtin.hide_first_space_indent_level
+    )
